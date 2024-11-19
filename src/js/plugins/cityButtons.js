fe780fa5024448bb5cb7384b63c50b58a23ad05c
Ana Carolina Alves Farias <ana.farias@compasso.com.br>
2024-11-19 13:17:20 -0300

    feat: adicionar filtros das fotos por categoria

--- src/js/plugins/cityButtons.js
+++ src/js/plugins/cityButtons.js
@@ -2,13 +2,47 @@ import $ from 'jquery'
 
 const duration = 600
 
-function filterByCity(city){
-    $('[wn-city').each(function(i,e) {
-        const isTarget = $(this).attr('wm-city') === city|| city === null
-        if(isTarget) {
+function filterByCity(city) {
+    $('[wn-city').each(function (i, e) {
+        const isTarget = $(this).attr('wm-city') === city || city === null
+        if (isTarget) {
+            $(this).parent().removeClass('d-nome')
             $(this).fadeIn(duration)
         } else {
-            $(this).fadeOut(duration)
+            $(this).fadeOut(duration, () => {
+                $(this).parent().addClass('d-nome')
+            })
         }
     })
 }
+
+const cityButtons = $('[wm-city-buttons]')
+
+$.fn.cityButtons = function() {
+    
+const cities = new Set
+$('[wm-city]').each(function (i, e) {
+    cities.add($(e).attr('wn-city'))
+})
+
+const btns = Array.from(cities).map(city => {
+    const btn = $('<button>')
+        .addClass(['btn', 'btn-info']).html(city)
+    btn.click(e => filterByCity(city))
+    return btn
+})
+
+const btnAll = $('<button>')
+    .addClass(['btn', 'btn-info', 'active']).html('Todas')
+btnAll.click(e => filterByCity(null))
+btns.push(btnAll)
+
+const btnGroup = $('<div>').addClass(['btn-group'])
+btnGroup.append(btns)
+
+$(this).html(btnGroup)
+
+return this
+}
+
+$('[wm-city-buttons]').cityButtons()
